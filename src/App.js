d23c4213873197ad25c91a4b6b4be9409beffdb5
Clem <clemence.fauchet23@gmail.com>
2020-02-13 20:31:04 +0100

    Basic styling for components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
+
 import './styles/App.scss'
 
 function App() {
-  return <div>Welcome to my webpack quiz!!</div>
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='title'>
+          <h1>Which Hogwarts house do you belong to?</h1>
+        </div>
+        <div className='houses'></div>
+      </div>
+
+      <div className='quiz-container'>
+        <div className='progress'>
+          <h2>Question 1 of 15</h2>
+        </div>
+        <div className='questions'>Question 1</div>
+        <div className='answers'>
+          <span>Rep 1</span>
+          <span>Rep 2</span>
+          <span>Rep 3</span>
+          <span>Rep 4</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
