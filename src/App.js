4ba9ec007e9a7713a9794a55273b5e10d6bf9c0f
Clem <clemence.fauchet23@gmail.com>
2020-02-13 18:30:53 +0100

    Updating ebpack config for sass url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles/App.scss'
 
 function App() {
   return <div>Welcome to my webpack quiz!!</div>
--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,47 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader', // compiles JSX(ES6)
-           // Creates `style` nodes from JS strings
-           'style-loader',
-           // Translates CSS into CommonJS
-           'css-loader',
-           // Compiles Sass to CSS
-           'sass-loader',
+        },
+      },
+
+      {
+        test: /\.s?css$/,
+        use: [
+          { loader: 'style-loader' },
+
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+
+          {
+            loader: 'resolve-url-loader',
+          },
+
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        exclude: [path.resolve(__dirname, './node_modules')],
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            outputPath: '../',
+          },
         },
       },
     ],
